367570c9a687bbd68da18e7b24a97cfae7768e4e
david-chua <dp_chua@hotmail.com>
2019-09-20 01:46:51 -0400

    added config secret key

--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
+from decouple import config
+import dj_database_url
+
 MIDDLEWARE = [
   # 'django.middleware.security.SecurityMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
   # ...
 ]
 
+SECRET_KEY = config('SECRET_KEY')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
