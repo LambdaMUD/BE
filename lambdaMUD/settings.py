2c4408edd64a48a03b1c65a4ab436f29fea74ff6
david-chua <dp_chua@hotmail.com>
2019-09-20 02:02:29 -0400

    removed some lines

--- lambdaMUD/settings.py
+++ lambdaMUD/settings.py
@@ -1,16 +1,11 @@
 from decouple import config
 import dj_database_url
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MIDDLEWARE = [
   # 'django.middleware.security.SecurityMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
   # ...
 ]
-
-DEBUG = config('DEBUG', cast = bool)
-
-WSGI_APPLICATION = 'lambdaMUD.wsgi.application'
 
 SECRET_KEY = config('SECRET_KEY')
 STATIC_URL = '/static/'
