bbf543294309d1026c3330c9cf6399c3e6a249d8
Leila <nb.leila10@gmail.com>
2019-09-21 15:21:06 -0500

    Add room model

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path, re_path
 from django.conf.urls import url
 from rest_framework.authtoken import views
+# from . import api
 
 urlpatterns = [
     # path('', include('rest_auth.urls')),
     path('', views.obtain_auth_token),
     path('registration/', include('rest_auth.registration.urls')),
-    
+    # url('maze_init', api.maze_init)
 ]
--- lambdaMUD/models.py
+++ lambdaMUD/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Room(models.Model):
+    row = models.IntegerField(default=0)
+    column = models.IntegerField(default=0)
+    wall_n = models.BooleanField(default=True)
+    wall_s = models.BooleanField(default=True)
+    wall_e = models.BooleanField(default=True)
+    wall_w = models.BooleanField(default=True)
+
+    def knock_down_wall(self, other, wall):
+        """When building maze: Knock down the wall between rooms self and other."""
+        # A wall separates a pair of cells in the N-S or W-E directions.
+        wall_pairs = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
+
+        self[f"wall_{wall}"] = False
+        other[f"wall_{wall_pairs[wall]}"] = False
